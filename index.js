14fa7b42401ea7c19798ecc11d91bd659cf8f2a9
Shaikh Najmul Islam <najmulbge17@gmail.com>
2022-08-09 18:38:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(express.json())
+
+// connect with mongoose
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7g5fvvn.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log("Connnnnnnnnected")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+// mainroute
+app.get('/', (req, res) => {
+    console.log("connected")
+    res.send("I am ready to go")
+})
+
+// listen
+app.listen(port, () => {
+    console.log("Port", port);
+})
